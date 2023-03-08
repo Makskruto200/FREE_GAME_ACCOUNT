98a52a0e9900f7c8c86cc1c60e34e9788d1f7d0d
Makskruto200 <makskruto200@gmail.com>
2023-03-08 18:26:49 +0700

    Initial commit

---
+++ FREE_GAME_ACCOUNT/FREE_GAME_ACCOUNT/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for FREE_GAME_ACCOUNT project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FREE_GAME_ACCOUNT.settings')
+
+application = get_asgi_application()
